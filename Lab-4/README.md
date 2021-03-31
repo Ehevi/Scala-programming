1b73068e51188dbd24295f9cfb94e1431bc779d8
Ehevi <aga.cyra@wp.pl>
2021-03-31 17:17:13 +0200

    lab-4 start :seedling:

---
+++ Lab-4/README.md
@@ -0,0 +1,14 @@
+# Laboratorium 4.
+## 1. Inferencja typu wyniku metody rekurencyjnej
+## 2. Elementarne algorytmy “tablicowe”: iteracja vs. rekursja
+## 3. Rekursja - przepełnienie stosu
+## 4. Rekursja “ogonowa”/końcowa, adnotacja @tailrec
+## 5. Usprawnianie procesów rekurencyjnych: “memoization“ i wzorzec “trampolina“
+## 6. Odwzorowanie `switch - case` (z Javy) w Scali (wariant z typem wyliczeniowym)
+## 7. Constant patterns (vs. variable patterns)
+## 8. Constructor patterns
+## 9. Variable binding and pattern guards
+## 10. Typed patterns
+## 11. Typ `Option`
+## 12. Ekstraktory
+#### Zadanie
---
+++ Lab-4/lab42.scala
@@ -0,0 +1,25 @@
+object Appl42 {
+  def checkPredicate(pred: Boolean,
+                     predAsString: String,
+                     prefix: String = "Checking if ") {
+    if (pred) println(prefix + predAsString + ": OK")
+    else println(prefix + predAsString + ": Fail")
+  }
+  def sumArrayIter(elems: Array[Int]) = {
+    var sum = 0
+    for (i <- elems) sum += i
+    sum
+  }
+  def main(args: Array[String]) {
+    val a1To5 = (1 to 5).toArray
+    println("Testing with a1To5 = " +
+            a1To5.mkString("Array(", ", ", ") ..."))
+    val expectResult = 15
+    checkPredicate(sumArrayIter(a1To5) == expectResult,
+                   "sumArrayIter(0, a1To5) == " + expectResult)
+    checkPredicate(sumArrayRec1(0, a1To5) == expectResult,
+                   "sumArrayRec1(0, a1To5) == " + expectResult)
+    checkPredicate(sumArrayRec2(a1To5) == expectResult,
+                   "sumArrayRec2(a1To5) == " + expectResult)
+  }
+}

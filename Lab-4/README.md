25ea0a81c1836cfdcbd57800f2ff80be595d7076
Ehevi <aga.cyra@wp.pl>
2021-04-15 14:50:18 +0200

    lab-4 finished :ice_cream:

--- Lab-4/README.md
+++ Lab-4/README.md
@@ -1,6 +1,6 @@
 # Laboratorium 4.
 ## 1. Inferencja typu wyniku metody rekurencyjnej
-### Plik scala41.scala:
+#### `lab41.scala`:
 ```scala
 object Appl41 {
   def factorial(n: Int): Int = {
@@ -21,8 +21,130 @@ object Appl41 {
   }
 }
 ```
-![](pictures/1-scala-appl41.png)
+<p align="center">
+  <img src="pictures/1-appl41.png" />
+</p>
+
+![](pictures/1-appl41.png)
+
 ## 2. Elementarne algorytmy “tablicowe”: *iteracja* vs. *rekursja*
+#### `lab41.scala`:
+```scala
+object Appl42 {
+  def checkPredicate(pred: Boolean,
+                     predAsString: String,
+                     prefix: String = "Checking if ") {
+    if (pred) println(prefix + predAsString + ": OK")
+    else println(prefix + predAsString + ": Fail")
+  }
+  def sumArrayIter(elems: Array[Int]) = {
+    var sum = 0
+    for (i <- elems) sum += i
+    sum
+  }
+  def sumArrayRec1(i: Int, elems: Array[Int]): Int = 0
+  def sumArrayRec2(elems: Array[Int]) = 0
+  def main(args: Array[String]) {
+    val a1To5 = (1 to 5).toArray
+    println("Testing with a1To5 = " +
+            a1To5.mkString("Array(", ", ", ") ..."))
+    val expectResult = 15
+    checkPredicate(sumArrayIter(a1To5) == expectResult,
+                   "sumArrayIter(0, a1To5) == " + expectResult)
+    checkPredicate(sumArrayRec1(0, a1To5) == expectResult,
+                   "sumArrayRec1(0, a1To5) == " + expectResult)
+    checkPredicate(sumArrayRec2(a1To5) == expectResult,
+                   "sumArrayRec2(a1To5) == " + expectResult)
+  }
+}
+```
+![](pictures/2-scala-appl42.png)
+
+#### Zadanie
+Uzupełnić definicje metod `sumArrayRec1` i `sumArrayRec2` tak, aby wszystkie testy "przechodziły pozytywnie" (miały status `OK`).
+```scala
+def sumArrayRec1(i: Int, elems: Array[Int]): Int = {
+  if (i < elems.size) elems(i) + sumArrayRec1(i + 1, elems)
+  else 0
+}
+  
+def sumArrayRec2(elems: Array[Int]) = {
+  val size = elems.size
+  def goFrom(i: Int): Int = {
+    if (i < size) elems(i) + goFrom(i + 1)
+    else 0
+  }
+  goFrom(0)
+}
+```
+![](pictures/2-4-scala-appl42.png)
+
+#### Zadanie
+Dodać zestawy testów oraz implementacje metod:
+- [x] `sumSqrArrayIter` + `sumSqrArrayRec1` + `sumSrqArrayRec2` (suma kwadratów elementów tablicy)
+- [x] `prodArrayIter` + `prodArrayRec1` + `prodArrayRec2` (iloczyn elementów tablicy)
+- [x] `sumAbsArrayIter` + `sumAbsArrayRec1` + `sumAbsArrayRec2` (suma wartości bezwzględnych elementów tablicy)
+
+```scala
+def sumSqrArrayIter(elems: Array[Int]) = {
+  var sum = 0
+  for (i <- elems) sum += pow(i, 2).intValue
+  sum
+}
+
+def sumSqrArrayRec1(i: Int, elems: Array[Int]): Int = {
+  if (i < elems.size) pow(elems(i), 2).intValue + sumSqrArrayRec1(i + 1, elems)
+  else 0
+}
+
+def sumSqrArrayRec2(elems: Array[Int]) = {
+  val size = elems.size
+  def goFrom(i: Int): Int = {
+    if (i < size) pow(elems(i), 2).intValue + goFrom(i + 1)
+    else 0
+  }
+  goFrom(0)
+}
+def prodArrayIter(elems: Array[Int]) = {
+  var prod = 1
+  for (i <- elems) prod *= i
+  prod
+}
+def prodArrayRec1(i: Int, elems: Array[Int]): Int = {
+  if (i < elems.size) elems(i) * prodArrayRec1(i + 1, elems)
+  else 1
+}
+def prodArrayRec2(elems: Array[Int]) = {
+  val size = elems.size
+  def goFrom(i: Int): Int = {
+    if (i < size) elems(i) * goFrom(i + 1)
+    else 1
+  }
+  goFrom(0)
+}
+def sumAbsArrayIter(elems: Array[Int]) = {
+  var sum = 0
+  for (i <- elems) sum += i.abs
+  sum
+}
+def sumAbsArrayRec1(i: Int, elems: Array[Int]): Int = {
+  if (i < elems.size) elems(i).abs + sumAbsArrayRec1(i + 1, elems)
+  else 0
+}
+def sumAbsArrayRec2(elems: Array[Int]) = {
+  val size = elems.size
+  def goFrom(i: Int): Int = {
+    if (i < size) elems(i).abs + goFrom(i + 1)
+    else 0
+  }
+  goFrom(0)
+}
+```
+![](pictures/2-42-scala-appl42.png)
+
+#### Zadanie
+uzupełnić brakujące miejsca w pliku `lab43.scala` tak, aby wszystkie testy przechodziły pozytywnie; metody mają zwracać element o największej wartości.
+
 ## 3. Rekursja - przepełnienie stosu
 ## 4. Rekursja “ogonowa”/końcowa, adnotacja *@tailrec*
 ## 5. Usprawnianie procesów rekurencyjnych: “*memoization*“ i wzorzec “*trampolina*“
--- Lab-4/lab42.scala
+++ Lab-4/lab42.scala
@@ -1,3 +1,6 @@
+import scala.math.pow
+import scala.annotation.tailrec
+
 object Appl42 {
   def checkPredicate(pred: Boolean,
                      predAsString: String,
@@ -5,21 +8,143 @@ object Appl42 {
     if (pred) println(prefix + predAsString + ": OK")
     else println(prefix + predAsString + ": Fail")
   }
+  
   def sumArrayIter(elems: Array[Int]) = {
     var sum = 0
     for (i <- elems) sum += i
     sum
   }
+  
+  def sumArrayRec1(i: Int, elems: Array[Int]): Int = {
+    if (i < elems.size) elems(i) + sumArrayRec1(i + 1, elems)
+    else 0
+  }
+  
+  def sumArrayRec2(elems: Array[Int]) = {
+    val size = elems.size
+    def goFrom(i: Int): Int = {
+      if (i < size) elems(i) + goFrom(i + 1)
+      else 0
+    }
+    goFrom(0)
+  }
+  
+  def sumSqrArrayIter(elems: Array[Int]) = {
+    var sum = 0
+    for (i <- elems) sum += pow(i, 2).intValue
+    sum
+  }
+  
+  def sumSqrArrayRec1(i: Int, elems: Array[Int]): Int = {
+    if (i < elems.size) pow(elems(i), 2).intValue + sumSqrArrayRec1(i + 1, elems)
+    else 0
+  }
+  
+  def sumSqrArrayRec2(elems: Array[Int]) = {
+    val size = elems.size
+    def goFrom(i: Int): Int = {
+      if (i < size) pow(elems(i), 2).intValue + goFrom(i + 1)
+      else 0
+    }
+    goFrom(0)
+  }
+
+  def sumSqrArrayRec3(elems: Array[Int]) = {
+    val size = elems.size
+    @tailrec
+    def goFrom(i: Int, acc: Int): Int = {
+      if(i < size) goFrom(i + 1, acc + pow(elems(i), 2).intValue)
+      else acc
+    }
+    goFrom(0, 0)
+  }
+
+  def prodArrayIter(elems: Array[Int]) = {
+    var prod = 1
+    for (i <- elems) prod *= i
+    prod
+  }
+
+  def prodArrayRec1(i: Int, elems: Array[Int]): Int = {
+    if (i < elems.size) elems(i) * prodArrayRec1(i + 1, elems)
+    else 1
+  }
+
+  def prodArrayRec2(elems: Array[Int]) = {
+    val size = elems.size
+    def goFrom(i: Int): Int = {
+      if (i < size) elems(i) * goFrom(i + 1)
+      else 1
+    }
+    goFrom(0)
+  }
+
+  def sumAbsArrayIter(elems: Array[Int]) = {
+    var sum = 0
+    for (i <- elems) sum += i.abs
+    sum
+  }
+
+  def sumAbsArrayRec1(i: Int, elems: Array[Int]): Int = {
+    if (i < elems.size) elems(i).abs + sumAbsArrayRec1(i + 1, elems)
+    else 0
+  }
+
+  def sumAbsArrayRec2(elems: Array[Int]) = {
+    val size = elems.size
+    def goFrom(i: Int): Int = {
+      if (i < size) elems(i).abs + goFrom(i + 1)
+      else 0
+    }
+    goFrom(0)
+  }
+
   def main(args: Array[String]) {
     val a1To5 = (1 to 5).toArray
-    println("Testing with a1To5 = " +
+    println("Testing sumArrayIter with a1To5 = " +
             a1To5.mkString("Array(", ", ", ") ..."))
-    val expectResult = 15
+    var expectResult = 15
     checkPredicate(sumArrayIter(a1To5) == expectResult,
                    "sumArrayIter(0, a1To5) == " + expectResult)
     checkPredicate(sumArrayRec1(0, a1To5) == expectResult,
                    "sumArrayRec1(0, a1To5) == " + expectResult)
     checkPredicate(sumArrayRec2(a1To5) == expectResult,
                    "sumArrayRec2(a1To5) == " + expectResult)
+
+    println("")
+    println("Testing sumSqrArrayIter with a1To5 = " +
+            a1To5.mkString("Array(", ", ", ") ..."))
+    expectResult = 55
+    checkPredicate(sumSqrArrayIter(a1To5) == expectResult,
+                   "sumSqrArrayIter(0, a1To5) == " + expectResult)
+    checkPredicate(sumSqrArrayRec1(0, a1To5) == expectResult,
+                   "sumSqrArrayRec1(0, a1To5) == " + expectResult)
+    checkPredicate(sumSqrArrayRec2(a1To5) == expectResult,
+                   "sumSqrArrayRec2(a1To5) == " + expectResult)
+    checkPredicate(sumSqrArrayRec3(a1To5) == expectResult,
+                   "sumSqrArrayRec3(a1To5) == " + expectResult)
+
+    println("")
+    println("Testing prodArrayIter with a1To5 = " +
+            a1To5.mkString("Array(", ", ", ") ..."))
+    expectResult = 120
+    checkPredicate(prodArrayIter(a1To5) == expectResult,
+                   "prodArrayIter(0, a1To5) == " + expectResult)
+    checkPredicate(prodArrayRec1(0, a1To5) == expectResult,
+                   "prodArrayRec1(0, a1To5) == " + expectResult)
+    checkPredicate(prodArrayRec2(a1To5) == expectResult,
+                   "prodArrayRec2(a1To5) == " + expectResult)
+
+    println("")
+    val aNegative1To5 = (-5 to -1).toArray
+    println("Testing sumAbsArrayIter with aNegative1To5 = " +
+            aNegative1To5.mkString("Array(", ", ", ") ..."))
+    expectResult = 15
+    checkPredicate(sumAbsArrayIter(aNegative1To5) == expectResult,
+                   "sumAbsArrayIter(0, aNegative1To5) == " + expectResult)
+    checkPredicate(sumAbsArrayRec1(0, aNegative1To5) == expectResult,
+                   "sumAbsArrayRec1(0, aNegative1To5) == " + expectResult)
+    checkPredicate(sumAbsArrayRec2(aNegative1To5) == expectResult,
+                   "sumAbsArrayRec2(aNegative1To5) == " + expectResult)
   }
 }

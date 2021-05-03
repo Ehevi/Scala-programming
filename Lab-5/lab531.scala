33d862c422eb483c1b0fd2a09e0838086a601003
Ehevi <aga.cyra@wp.pl>
2021-05-03 16:07:42 +0200

    lab-5 finished :turtle:

--- Lab-5/lab531.scala
+++ Lab-5/lab531.scala
@@ -1,4 +1,5 @@
 import scala.math.pow
+
 object Appl531 {
   def genSumArray(elems: Array[Int], f: (Int) => Int) = {
     var sum = 0
@@ -8,8 +9,17 @@ object Appl531 {
 
   def sum(elems: Array[Int]) = genSumArray(elems, (e: Int) => e)
 
+  def sumSqr(elems: Array[Int]) = genSumArray(elems, (e: Int) => pow(e, 2).intValue)
+
+  def sumCube(elems: Array[Int]) = genSumArray(elems, (e: Int) => pow(e, 3).intValue)
+
+  def sumAbs(elems: Array[Int]) = genSumArray(elems, (e: Int) => e.abs)
+
   def main(args: Array[String]) {
     val a = Array(1,2,3,4,-5)
     println("sum(a) = " + sum(a))
+    println("sumSqr(a) = " + sumSqr(a))
+    println("sumCube(a) = " + sumCube(a))
+    println("sumAbs(a) = " + sumAbs(a))
   }
 }
